e98ad1bc20fe7f58fbfdb3025c8cb8036bffa2b5
LiJiaCheng <2530551603@qq.com>
2019-05-29 15:20:23 +0800

    加了一个ID

--- app/src/main/java/com/example/dome/MainActivity.java
+++ app/src/main/java/com/example/dome/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.dome;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Hello World!
+     */
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mTextView = (TextView) findViewById(R.id.TextView);
     }
 }
